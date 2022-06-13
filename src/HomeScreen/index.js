4f1c9182b7032e1e4d56c1073c99b4d195437a29
Justice Velasco <justicevgomez001@gmail.com>
2022-06-13 16:57:53 +0200

    ✨ CREATED: Input call api

--- src/HomeScreen/index.js
+++ src/HomeScreen/index.js
@@ -2,20 +2,46 @@ import React, { useEffect, useState } from "react";
 import { getWordInfo } from "../data/api";
 
 function HomeScreen() {
-  const [word, setWord] = useState([]);
+  const [word, setWord] = useState({
+    data: [],
+    searchTerm: "",
+  });
 
   const fetchData = async () => {
     const fetchedData = await getWordInfo();
-    setWord(fetchedData);
+    if (fetchedData) {
+      setWord({
+        data: fetchData,
+      });
+    }
   };
 
   useEffect(() => {
-    fetchData()
-  }, [])
-  
-  console.log(word)
+    fetchData();
+  }, []);
 
-  return <div>- {word.word}</div>;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await getWordInfo(word.searchTerm);
+    return setWord({
+      data: response,
+      searchTerm: "",
+    });
+  };
+  const { data } = word;
+
+  console.log(data)
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search"
+        value={word.searchTerm}
+        onChange={(e) => setWord({ ...word, searchTerm: e.target.value })}
+        autoFocus
+      />
+      <h1>{data.word}</h1>
+    </form>
+  );
 }
-
 export default HomeScreen;
--- src/data/api.js
+++ src/data/api.js
@@ -1,6 +1,6 @@
-export const getWordInfo = async () => {
+export const getWordInfo = async (word) => {
   try {
-    const url = "https://api.dictionaryapi.dev/api/v2/entries/en/hello";
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     const fetchWordInfo = await fetch(url);
     const wordInfo = await fetchWordInfo.json();
     return wordInfo[0];
