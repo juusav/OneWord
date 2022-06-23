3e6fb6c0b5acc511fc4443fabd5fc9e565cde87a
Justice Velasco <justicevgomez001@gmail.com>
2022-06-23 17:32:05 +0200

    ✨ CREATED: Axios installation

--- src/data/api.js
+++ src/data/api.js
@@ -1,7 +1,9 @@
+import axios from "axios";
+
 export const getWordInfo = async (word = "word") => {
   try {
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-    const fetchWordInfo = await fetch(url);
+    const fetchWordInfo = await axios.get(url);
     const wordInfo = await fetchWordInfo.json();
     return wordInfo[0];
   } catch (e) {
